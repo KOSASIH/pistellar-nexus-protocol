96642f8cf0fab0faab57ae9ba89db89b2efecab9
KOSASIH <kosasihg88@gmail.com>
2025-03-26 15:56:11 +0700

    Update pi_integration.rs

--- src/cross_chain/pi_integration.rs
+++ src/cross_chain/pi_integration.rs
@@ -1,8 +1,9 @@
 use serde::{Deserialize, Serialize};
 use reqwest::Client;
 use std::error::Error;
+use log::{info, error};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct PiTransaction {
     pub sender: String,
     pub receiver: String,
@@ -15,6 +16,7 @@ pub struct PiIntegration {
 }
 
 impl PiIntegration {
+    /// Creates a new instance of PiIntegration with the specified API URL.
     pub fn new(api_url: &str) -> Self {
         let client = Client::new();
         Self {
@@ -23,7 +25,10 @@ impl PiIntegration {
         }
     }
 
+    /// Sends a transaction to the Pi network.
     pub async fn send_transaction(&self, transaction: PiTransaction) -> Result<String, Box<dyn Error>> {
+        info!("Sending transaction: {:?}", transaction);
+        
         let response = self.client.post(&self.pi_api_url)
             .json(&transaction)
             .send()
@@ -31,22 +36,31 @@ impl PiIntegration {
 
         if response.status().is_success() {
             let tx_id: String = response.json().await?;
+            info!("Transaction sent successfully, ID: {}", tx_id);
             Ok(tx_id)
         } else {
-            Err("Failed to send transaction".into())
+            let error_message = format!("Failed to send transaction: {}", response.status());
+            error!("{}", error_message);
+            Err(error_message.into())
         }
     }
 
+    /// Retrieves the status of a transaction by its ID.
     pub async fn get_transaction_status(&self, tx_id: &str) -> Result<String, Box<dyn Error>> {
+        info!("Retrieving status for transaction ID: {}", tx_id);
+        
         let response = self.client.get(format!("{}/{}", self.pi_api_url, tx_id))
             .send()
             .await?;
 
         if response.status().is_success() {
             let status: String = response.json().await?;
+            info!("Transaction status retrieved successfully: {}", status);
             Ok(status)
         } else {
-            Err("Failed to retrieve transaction status".into())
+            let error_message = format!("Failed to retrieve transaction status: {}", response.status());
+            error!("{}", error_message);
+            Err(error_message.into())
         }
     }
 }
