e1af91528902e6c15b9459e018d5081af02a5d9c
KOSASIH <kosasihg88@gmail.com>
2025-03-26 15:49:12 +0700

    Update mod.rs

--- src/analytics/mod.rs
+++ src/analytics/mod.rs
@@ -1,2 +1,4 @@
-pub mod analytics;
-pub mod analytics_tests;
+// src/mod.rs
+
+pub mod analytics;        // Include the analytics module
+pub mod analytics_tests;  // Include the analytics tests module
